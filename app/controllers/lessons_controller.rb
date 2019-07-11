5c956fa76af878931eda7ebc0fbcbcb2957475a8
Majun1990 <merijun_a@hotmail.com>
2019-07-11 21:53:28 +0000

    name error

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_lesson, only: [:show]
+  
   def show
-    
+
   end
 
   private
+
+  def require_enrolled_in_current_lesson
+    if current_lesson.section.course.user != current_user.enrolled_in?
+      redirect_to course_path(current_course), alert: 'You are not enrolled'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])    
   end
+
 end
